5eea315326d1485bdb190cb1d710273a73d2262d
OsmauGon <mauricio7892@hotmail.com>
2026-08-09 17:21:42 -0300

    Fallamos el primer get endpoint

--- src/backend-endpoints.ts
+++ src/backend-endpoints.ts
@@ -1,5 +1,5 @@
-//export const profesionalesGet: string = "profesionales.txt";
-export const profesionalesGet: string = "https://vanesabackend-production-a947.up.railway.app/api/profes/private";
+export const profesionalesGet: string = "profesionales.txt";
+//export const profesionalesGet: string = "https://vanesabackend-production-a947.up.railway.app/api/profes/private";
 export const establecimientosGet: string = "establecimientos.txt";
 export const noticiasGet: string = "urlparasolicitaralasnoticias";
 export const blogsGet: string = "urlparasolicitaralosblogsdeveterinarios";
